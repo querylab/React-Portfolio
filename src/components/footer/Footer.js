729a5d7057ddc2efb8912a7a0314d7b214a2693b
PresenteApp-v1.0 <emygonzalez@tutanota.com>
2023-02-26 20:37:44 -0400

    Added Umami Analytics

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,6 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer>
+      <script async defer data-website-id="d9691128-7b5b-495a-b43c-1495fd627698" src="https://umami-54r3.onrender.com/umami.js"></script>
       <p className="copyright">
         Made with{' '}
         <span role="img" aria-label="thinking">
